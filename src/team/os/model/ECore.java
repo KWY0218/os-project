24e8e59c003dd7358671a151302f63c42a1c9c2d
Dev-SaTu <tkdwns1610@naver.com>
2022-05-03 04:01:00 +0900

    #10 [REFACTOR] ECore

--- src/team/os/model/ECore.java
+++ src/team/os/model/ECore.java
@@ -2,7 +2,7 @@ package team.os.model;
 
 public class ECore extends Core{
 	public ECore(){
-		super.setPower(2);
-		super.setPowerConsumption(3);
+		super.setPower(1);
+		super.setPowerConsumption(1);
 	}
 }
