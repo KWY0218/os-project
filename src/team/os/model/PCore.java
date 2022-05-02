a2ee994c4950f286d7f1fa1c93f38b6d74823854
Dev-SaTu <tkdwns1610@naver.com>
2022-05-03 04:01:13 +0900

    #10 [REFACTOR] PCore

--- src/team/os/model/PCore.java
+++ src/team/os/model/PCore.java
@@ -2,7 +2,7 @@ package team.os.model;
 
 public class PCore extends Core{
 	public PCore(){
-		super.setPower(1);
-		super.setPowerConsumption(1);
+		super.setPower(2);
+		super.setPowerConsumption(3);
 	}
 }
