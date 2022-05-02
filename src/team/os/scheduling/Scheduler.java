1cacd25568e2ff3284a3991f619d39a940988cd6
KWY0218 <wodrk1001@gmail.com>
2022-05-03 02:42:07 +0900

    hotfix edit import in process

--- src/team/os/scheduling/Scheduler.java
+++ src/team/os/scheduling/Scheduler.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import team.os.model.Core;
 import team.os.model.History;
+import team.os.model.Process;
 
 public interface Scheduler {
 	public History schedule(List<Process> mProcessList, List<Core> mCoreList);
