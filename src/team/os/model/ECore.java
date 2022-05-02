156cc51c7fb938adafdbd27d04fdb1aa65089103
KWY0218 <wodrk1001@gmail.com>
2022-05-03 01:40:50 +0900

    #1 [ADD] 코어 추가

---
+++ src/team/os/model/ECore.java
@@ -0,0 +1,8 @@
+package team.os.model;
+
+public class ECore extends Core{
+	public ECore(){
+		super.setPower(2);
+		super.setPowerConsumption(3);
+	}
+}
---
+++ src/team/os/model/PCore.java
@@ -0,0 +1,8 @@
+package team.os.model;
+
+public class PCore extends Core{
+	public PCore(){
+		super.setPower(1);
+		super.setPowerConsumption(1);
+	}
+}
