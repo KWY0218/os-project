10329c6926939d17c33e2f0062f88f910c11ebd2
Dev-SaTu <tkdwns1610@naver.com>
2022-05-05 22:08:21 +0900

    Hotfix TestScheduling

--- src/team/os/test/TestScheduling.java
+++ src/team/os/test/TestScheduling.java
@@ -18,99 +18,107 @@ public class TestScheduling {
 
 	public static void main(String[] args) {
 
-		// 프로세스 리스트를 생성한다.
-		List<Process> processList = new ArrayList<Process>();
+		for(int j = 0; j < 1000; j++) {
 
-		for(int i = 0; i < 15; i++)
+			// 프로세스 리스트를 생성한다.
+			List<Process> processList = new ArrayList<Process>();
 
-			processList.add(new Process(processList.size() + 1, new Random().nextInt(15), new Random().nextInt(5) + 1));
+			for(int i = 0; i < 15; i++)
 
-		// 프로세스 리스트를 정렬한다.
-		processList.sort(Comparator.naturalOrder());
+				processList.add(new Process(processList.size() + 1, new Random().nextInt(15), new Random().nextInt(5) + 1));
 
-		// 코어 리스트를 생성한다.
-		List<Core> coreList = new LinkedList<Core>();
+			// 프로세스 리스트를 정렬한다.
+			processList.sort(Comparator.naturalOrder());
 
-		for(int i = 0; i < CPU.MAX_CORE_SIZE; i++)
+			// 코어 리스트를 생성한다.
+			List<Core> coreList = new LinkedList<Core>();
 
-			// case 2는 disable
-			switch(new Random().nextInt(3)) {
+			for(int i = 0; i < CPU.MAX_CORE_SIZE; i++)
 
-			case 0: coreList.add(new ECore()); break;
-			case 1: coreList.add(new PCore()); break;
+				// case 2는 disable
+				switch(new Random().nextInt(3)) {
 
-			}
+				case 0: coreList.add(new ECore()); break;
+				case 1: coreList.add(new PCore()); break;
 
-		// 예제 프로세스 및 코어
-		processList.clear();
-		processList.add(new Process(1, 0, 3));
-		processList.add(new Process(2, 1, 7));
-		processList.add(new Process(3, 3, 2));
-		processList.add(new Process(4, 5, 5));
-		processList.add(new Process(5, 6, 3));
+				}
 
-		coreList.clear();
-		coreList.add(new ECore());
-		coreList.add(new PCore());
+			if(coreList.isEmpty())
 
-		// 프로세스 및 코어 리스트를 출력한다.
-		History history = new HRRN().schedule(processList, coreList);
+				coreList.add(new ECore());
 
-		System.out.println("-- Core");
+			// 예제 프로세스 및 코어
+//			processList.clear();
+//			processList.add(new Process(1, 0, 3));
+//			processList.add(new Process(2, 1, 7));
+//			processList.add(new Process(3, 3, 2));
+//			processList.add(new Process(4, 5, 5));
+//			processList.add(new Process(5, 6, 3));
+//
+//			coreList.clear();
+//			coreList.add(new ECore());
+//			coreList.add(new PCore());
 
-		for(Core core : coreList)
+			// 프로세스 및 코어 리스트를 출력한다.
+			History history = new SRTN().schedule(processList, coreList);
 
-			System.out.printf("Core %d\t%s\n", coreList.indexOf(core) + 1, core.getClass().getSimpleName());
+			System.out.println("-- Core");
 
-		System.out.println("-- Process State");
+			for(Core core : coreList)
 
-		for(Process process : processList)
+				System.out.printf("Core %d\t%s\n", coreList.indexOf(core) + 1, core.getClass().getSimpleName());
 
-			System.out.printf("P%02d\t%2d\t%2d\t%2d\t%2d\t%4.1f\n", process.getpId(), process.getArrivalTime(), process.getBurstTime(), process.getWaitingTime(), process.getTurnAroundTime(), process.getNormalizedTT());
+			System.out.println("-- Process State");
 
-		// 히스토리 테스트
-		System.out.println("-- Gantt");
+			for(Process process : processList)
 
-		System.out.print("       ");
+				System.out.printf("P%02d\t%2d\t%2d\t%2d\t%2d\t%4.1f\n", process.getpId(), process.getArrivalTime(), process.getBurstTime(), process.getWaitingTime(), process.getTurnAroundTime(), process.getNormalizedTT());
 
-		for(int historyIndex = 0; historyIndex < history.getHistory().size(); historyIndex++)
+			// 히스토리 테스트
+			System.out.println("-- Gantt");
 
-			System.out.printf("%4d ", historyIndex);
+			System.out.print("       ");
 
-		System.out.println();
+			for(int historyIndex = 0; historyIndex < history.getHistory().size(); historyIndex++)
 
-		for(int coreIndex = 0; coreIndex < coreList.size(); coreIndex++) {
-
-			System.out.printf("Core%2d", coreIndex + 1);
-
-			for(List<Process> pl : history.getHistory()) {
-
-				boolean worked = false;
-
-				for(Process p : pl)
-
-					if(p.getWorkingCoreIndex() == coreIndex) {
-
-						System.out.printf("%5d", p.getpId());
-
-						worked = true;
-
-					}
-
-				if(!worked)
-
-					System.out.printf("     ");
-
-			}
+				System.out.printf("%4d ", historyIndex);
 
 			System.out.println();
 
+			for(int coreIndex = 0; coreIndex < coreList.size(); coreIndex++) {
+
+				System.out.printf("Core%2d", coreIndex + 1);
+
+				for(List<Process> pl : history.getHistory()) {
+
+					boolean worked = false;
+
+					for(Process p : pl)
+
+						if(p.getWorkingCoreIndex() == coreIndex) {
+
+							System.out.printf("%5d", p.getpId());
+
+							worked = true;
+
+						}
+
+					if(!worked)
+
+						System.out.printf("     ");
+
+				}
+
+				System.out.println();
+
+			}
+
+			System.out.println("-- Total");
+			System.out.printf("History.getTotalBurstTime(): %d\n", history.getTotalBurstTime());
+			System.out.printf("History.getTotalPowerConsumption(): %.1f\n", history.getTotalPoewrConsumption());
+
 		}
-		
-		System.out.println("-- Total");
-		System.out.printf("History.getTotalBurstTime(): %d\n", history.getTotalBurstTime());
-		System.out.printf("History.getTotalPowerConsumption(): %.1f\n", history.getTotalPoewrConsumption());
 
 	}
-	
+
 }
