3da80c064340c8f2bc634230d9a6ad87d9dcf8ce
KWY0218 <wodrk1001@gmail.com>
2022-05-05 17:37:57 +0900

    #27 [REFACTOR] SPN, HRRN, SRTN 수정

--- src/team/os/scheduling/HRRN.java
+++ src/team/os/scheduling/HRRN.java
@@ -16,13 +16,14 @@ public class HRRN implements Scheduler{
 	 * 	1. 모든 프로세스가 종료할 때 까지 반복한다.
 	 * 	2. 현재시간과 AT가 같은 프로세스들을 readyList 안에 넣는다.
 	 * 	3. readyList 안에 있는 값을 age 순으로 정렬한다.
-	 * 	4. 코어가 남아있고 && readyList 안에 프로세스가 있다면 계속 readyList 맨 앞에 있는 프로세스에 코어를 할당한다.
-	 * 	5. 코어 할당이 끝난 후 일하고 있는 코어의 누적 전력량을 갱신한다.
+	 *  4. 일이 끝난 코어를 비운다.
+	 * 	5. 코어가 남아있고 && readyList 안에 프로세스가 있다면 계속 readyList 맨 앞에 있는 프로세스에 코어를 할당한다.
 	 * 	6. readyList 내에 있는 프로세스들의 age를 갱신한다.
 	 * 	7. 코어를 할당받은 프로세스들을 일시킨다.
 	 * 	8. 1초 동안 mProcessList와 mCoreList의 변경사항을 history에 넣는다.
 	 * 	9. 1초를 증가한다.
-	 * 	10. 프로세스 동작이 끝난 후 총 전력량과 소비시간을 history에 넣는다.
+	 * 	10. 코어 할당이 끝난 후 일하고 있는 코어의 누적 전력량을 갱신한다.
+	 * 	11. 프로세스 동작이 끝난 후 총 전력량과 소비시간을 history에 넣는다.
 	 */
 	@Override
 	public History schedule(List<Process> mProcessList, List<Core> mCoreList) {
@@ -40,26 +41,34 @@ public class HRRN implements Scheduler{
 			// 3. readyList 안에 있는 값을 age 순으로 정렬한다.
 			readyList.sort(Comparator.comparing(Process::getAge));
 			
-			// 4. 코어가 남아있고 && readyList 안에 프로세스가 있다면 readyList 맨 앞에 있는 프로세스에 코어를 할당한다. 둘 중 하나가 false가 나올 때 까지 반복한다.
-			while(CPU.getRecommendCore(mCoreList) != -1 && !readyList.isEmpty()) {
-				// 4. 코어가 남아있으니 코어를 추천받는다.
-				int coreIndex = CPU.getRecommendCore(mCoreList);
-				
-				// 4. readyList 에서 BT가 가장 짧은 프로세스 
+			// 4. 일이 끝난 코어를 비운다.
+			for(Process process:mProcessList) {
+				if(process.isTerminated() &&process.getWorkingCoreIndex()!=-1) {
+					// 4. 이 코어는 일을 다했다고 설정한다.
+					mCoreList.get(process.getWorkingCoreIndex()).setWorking(false);
+					
+					// 4. 이 프로세스는 일을 다했다고 설정한다.
+					process.setWorkingCoreIndex(-1);
+					
+				}
+			}
+			
+			// 5. 코어가 남아있고 && readyList 안에 프로세스가 있다면 readyList 맨 앞에 있는 프로세스에 코어를 할당한다. 둘 중 하나가 false가 나올 때 까지 반복한다.
+			while(CPU.getRecommendCore(mCoreList,-1) != -1 && !readyList.isEmpty()) {
+				// 5. readyList 에서 BT가 가장 짧은 프로세스 
 				Process currentProcess = readyList.get(0);
 				
-				// 4. 코어를 할당 받은 프로세스의 할당 받은 코어의 인덱스를 설정하고, waitingTime을 설정하고, working 중인 것을 명시한다. 
+				// 5. 코어가 남아있으니 코어를 추천받는다.
+				int coreIndex = CPU.getRecommendCore(mCoreList,currentProcess.getRemainBurstTime());
+				
+				// 5. 코어를 할당 받은 프로세스의 할당 받은 코어의 인덱스를 설정하고, waitingTime을 설정하고, working 중인 것을 명시한다. 
 				mProcessList.get(currentProcess.getpId()).setWorkingCoreIndex(coreIndex);
 				mProcessList.get(currentProcess.getpId()).setWaitingTime(currentTime-mProcessList.get(currentProcess.getpId()).getArrivalTime());
 				mCoreList.get(coreIndex).setWorking(true);
 				
-				// 4. 코어를 할당 받은 readyList 맨 앞 요소를 제거한다.
+				// 5. 코어를 할당 받은 readyList 맨 앞 요소를 제거한다.
 				readyList.remove(0);
 			}
-			
-			// 5. 코어 할당이 끝난 후 일하고 있는 코어의 누적 전력량을 갱신한다. cpu 에서 새로 만든 함수임
-			totalAccConsumption += CPU.getPowerConsumptionOfCoreList(mCoreList);
-			totalAccConsumption += CPU.getStandbyPowerOfCoreList(mCoreList);
 			
 			// 6. readyList에서 대기중인 프로세스들의 age를 증가한다. 
 			for(Process process:readyList) process.setAssignWaitingTime();
@@ -73,16 +82,11 @@ public class HRRN implements Scheduler{
 					process.setRemainBurstTime(process.getRemainBurstTime()-mCoreList.get(process.getWorkingCoreIndex()).getPower());
 					
 					// 7. 만약 remainBT 가 0 이하가 되면 프로세스와 코어가 일을 다했다고 설정한다. 
-					if(process.getRemainBurstTime()<=0) {
-						// 7. 이 코어는 일을 다했다고 설정한다.
-						mCoreList.get(process.getWorkingCoreIndex()).setWorking(false);
-						
-						// 7. 이 프로세스는 일을 다했다고 설정한다.
-						process.setWorkingCoreIndex(-1);
-						
+					if(process.getRemainBurstTime()<=0) {	
 						// 7. 프로세스가 종료 됐음을 명시하고, 일이 끝난 프로세스의 TT, NTT를 설정한다.
 						process.setTerminated(true);
 						process.setTurnAroundTime(currentTime-process.getArrivalTime());
+						process.setWaitingTime(process.getTurnAroundTime()-process.getBurstTime());
 						process.setNormalizedTT(process.getTurnAroundTime()/process.getBurstTime());
 					}
 				}
@@ -92,8 +96,12 @@ public class HRRN implements Scheduler{
 			
 			// 9. 1초 시간을 증가한다.
 			currentTime++;
+			
+			// 10. 코어 할당이 끝난 후 일하고 있는 코어의 누적 전력량을 갱신한다.
+			totalAccConsumption += CPU.getPowerConsumptionOfCoreList(mCoreList);
+			totalAccConsumption += CPU.getStandbyPowerOfCoreList(mCoreList);
 		}
-		// 10. 프로세스 동작이 끝난 후 총 전력량과 소비시간을 history에 넣는다.
+		// 11. 프로세스 동작이 끝난 후 총 전력량과 소비시간을 history에 넣는다.
 		history.setTotalBurstTime(currentTime);
 		history.setTotalPowerConsumption(totalAccConsumption);
 		return history;
--- src/team/os/scheduling/SRTN.java
+++ src/team/os/scheduling/SRTN.java
@@ -15,14 +15,15 @@ public class SRTN  implements Scheduler{
 	 * 	1. 모든 프로세스가 종료할 때 까지 반복한다.
 	 * 	2. 현재시간과 AT가 같은 프로세스들을 readyList 안에 넣는다.
 	 * 	3. readyList 안에 있는 값을 remainBT 순으로 정렬한다.
-	 * 	4. 코어가 남아있고 && readyList 안에 프로세스가 있다면 계속 readyList 맨 앞에 있는 프로세스에 코어를 할당한다.
-	 *  5. 코어가 가득 차있고, readyList에 프로세스가 있으면 remainBT를 계산해서 선점을 진행한다.
+	 * 	4. 일이 끝난 코어를 비운다.
+	 * 	5. 코어가 남아있고 && readyList 안에 프로세스가 있다면 계속 readyList 맨 앞에 있는 프로세스에 코어를 할당한다.
+	 *  6. 코어가 가득 차있고, readyList에 프로세스가 있으면 remainBT를 계산해서 선점을 진행한다.
 	 * 	7. 선점이 끝난 후 readyList를 remainBT 순으로 정렬한다.	
-	 * 	8. 코어 할당이 끝난 후 일하고 있는 코어의 누적 전력량을 갱신한다.
-	 * 	9. 코어를 할당받은 프로세스들을 일시킨다.
-	 * 	10. 1초 동안 mProcessList와 mCoreList의 변경사항을 history에 넣는다.
-	 * 	11. 1초를 증가한다.
-	 * 	12. 총 BT와 전력량을 history에 전달한다.
+	 * 	8. 코어를 할당받은 프로세스들을 일시킨다.
+	 * 	9. 1초 동안 mProcessList와 mCoreList의 변경사항을 history에 넣는다.
+	 * 	10. 1초를 증가한다.
+	 * 	11. 코어 할당이 끝난 후 일하고 있는 코어의 누적 전력량을 갱신한다.
+	 *  12. 총 BT와 전력량을 History 에 저장한다.
 	 */
 	@Override
 	public History schedule(List<Process> mProcessList, List<Core> mCoreList) {
@@ -41,25 +42,37 @@ public class SRTN  implements Scheduler{
 			// 3. readyList 안에 있는 값을 remainBurstTime 순으로 정렬한다.
 			readyList.sort(Comparator.comparing(Process::getRemainBurstTime));
 			
-			// 4. 코어가 남아있고 && readyList 안에 프로세스가 있다면 readyList 맨 앞에 있는 프로세스에 코어를 할당한다. 둘 중 하나가 false가 나올 때 까지 반복한다.
-			while(CPU.getRecommendCore(mCoreList) != -1 && !readyList.isEmpty()) {
-				// 4. 코어가 남아있으니 코어를 추천받는다.
-				int coreIndex = CPU.getRecommendCore(mCoreList);
+			// 4. 일이 끝난 코어를 비운다.
+			for(Process process:mProcessList) {
+				if(process.isTerminated() &&process.getWorkingCoreIndex()!=-1) {
+					// 4. 이 코어는 일을 다했다고 설정한다.
+					mCoreList.get(process.getWorkingCoreIndex()).setWorking(false);
+					
+					// 4. 이 프로세스는 일을 다했다고 설정한다.
+					process.setWorkingCoreIndex(-1);
+				}
+			}
+			
+			// 5. 코어가 남아있고 && readyList 안에 프로세스가 있다면 readyList 맨 앞에 있는 프로세스에 코어를 할당한다. 둘 중 하나가 false가 나올 때 까지 반복한다.
+			while(CPU.getRecommendCore(mCoreList,-1) != -1 && !readyList.isEmpty()) {
 				
-				// 4. readyList 에서 BT가 가장 짧은 프로세스 
+				// 5. readyList 에서 reaminBT가 가장 짧은 프로세스 
 				Process currentProcess = readyList.get(0);
 				
-				// 4. 코어를 할당 받은 프로세스의 할당 받은 코어의 인덱스를 설정하고,  working 중인 것을 명시한다. 
+				// 5. 코어가 남아있으니 코어를 추천받는다.
+				int coreIndex = CPU.getRecommendCore(mCoreList,currentProcess.getRemainBurstTime());
+				
+				// 5. 코어를 할당 받은 프로세스의 할당 받은 코어의 인덱스를 설정하고,  working 중인 것을 명시한다. 
 				mProcessList.get(currentProcess.getpId()).setWorkingCoreIndex(coreIndex);
 				mCoreList.get(coreIndex).setWorking(true);
 				
-				// 4. 코어를 할당 받은 readyList 맨 앞 요소를 제거한다.
+				// 5. 코어를 할당 받은 readyList 맨 앞 요소를 제거한다.
 				readyList.remove(0);
 			}
 
-			// 5. 코어가 가득 차있고, readyList에 프로세스가 있으면 remainBT를 계산해서 선점을 한다.
+			// 6. 코어가 가득 차있고, readyList에 프로세스가 있으면 remainBT를 계산해서 선점을 한다.
 			while(!readyList.isEmpty()) {
-				// 5. 코어 안에 있는 값 중 remainBT 가 가장 큰 프로세스를 찾는다.
+				// 6. 코어 안에 있는 값 중 remainBT 가 가장 큰 프로세스를 찾는다.
 				Process maxProcess = new Process(-1,-1,-1);
 				for(Process process:mProcessList) {
 					if(process.getWorkingCoreIndex() != -1) {
@@ -67,34 +80,30 @@ public class SRTN  implements Scheduler{
 					}
 				}
 				
-				// 5. readyList 최상단에 있는 프로세스의 remainBT와 방금 코어에서 찾은 최대 프로세스의 remainBT를 비교한다.
+				// 6. readyList 최상단에 있는 프로세스의 remainBT와 방금 코어에서 찾은 최대 프로세스의 remainBT를 비교한다.
 				if(maxProcess.getRemainBurstTime()>readyList.get(0).getRemainBurstTime()) {
-					// 5. 코어에서 찾은 프로세스의 BT가 더 크면, 해당 프로세스의 코어 인덱스를 readyList 최상단에 있는 프로세스에게 할당한다.
+					// 6. 코어에서 찾은 프로세스의 BT가 더 크면, 해당 프로세스의 코어 인덱스를 readyList 최상단에 있는 프로세스에게 할당한다.
 					readyList.get(0).setWorkingCoreIndex(maxProcess.getWorkingCoreIndex());
 					
-					// 5. mProcessList 에도 값을 설정한다.
+					// 6. mProcessList 에도 값을 설정한다.
 					mProcessList.get(readyList.get(0).getpId()).setWorkingCoreIndex(maxProcess.getWorkingCoreIndex());
 					
-					// 5. maxProcess 의 코어 값을 -1로 설정한다.
+					// 6. maxProcess 의 코어 값을 -1로 설정한다.
 					maxProcess.setWorkingCoreIndex(-1);
 					
-					// 5. mProcessList 에도 코어를 나오는 프로세스의 정보르 수정한다.   
+					// 6. mProcessList 에도 코어를 나오는 프로세스의 정보르 수정한다.   
 					mProcessList.get(maxProcess.getpId()).setWorkingCoreIndex(-1);
 					
-					// 5. maxProcess 를 readyList안에 넣는다.
+					// 6. maxProcess 를 readyList안에 넣는다.
 					readyList.add(maxProcess);
 					
-					// 5. 코어로 들어간 프로세스의 정보를 readyList 에서 없앤다.
+					// 6. 코어로 들어간 프로세스의 정보를 readyList 에서 없앤다.
 					readyList.remove(0);	
 				} else break;
 			}
 			
-			// 6. 다시 readyList를 remainBT 순으로 정렬한다.
+			// 7. 다시 readyList를 remainBT 순으로 정렬한다.
 			readyList.sort(Comparator.comparing(Process::getRemainBurstTime));
-			
-			// 7. 코어 할당이 끝난 후 일하고 있는 코어의 누적 전력량을 갱신한다.
-			totalAccConsumption += CPU.getPowerConsumptionOfCoreList(mCoreList);
-			totalAccConsumption += CPU.getStandbyPowerOfCoreList(mCoreList);
 			
 			// 8. 코어를 할당받은 프로세스들을 일시킨다.
 			for(Process process:mProcessList) {
@@ -106,12 +115,6 @@ public class SRTN  implements Scheduler{
 					
 					// 8. 만약 remainBT 가 0 이하가 되면 프로세스와 코어가 일을 다했다고 설정한다. 
 					if(process.getRemainBurstTime()<=0) {
-						// 8. 이 코어는 일을 다했다고 설정한다.
-						mCoreList.get(process.getWorkingCoreIndex()).setWorking(false);
-						
-						// 8. 이 프로세스는 일을 다했다고 설정한다.
-						process.setWorkingCoreIndex(-1);
-						
 						// 8. 프로세스가 종료 됐음을 명시하고, 일이 끝난 프로세스의 TT, WT, NTT를 설정한다.
 						process.setTerminated(true);
 						process.setTurnAroundTime(currentTime-process.getArrivalTime());
@@ -126,12 +129,14 @@ public class SRTN  implements Scheduler{
 			// 10. 1초 시간을 증가한다.
 			currentTime++;
 			
+			// 11. 코어 할당이 끝난 후 일하고 있는 코어의 누적 전력량을 갱신한다.
+			totalAccConsumption += CPU.getPowerConsumptionOfCoreList(mCoreList);
+			totalAccConsumption += CPU.getStandbyPowerOfCoreList(mCoreList);
 		}
 		
-		// 11. 총 BT와 전력량을 History 에 저장한다.
+		// 12. 총 BT와 전력량을 History 에 저장한다.
 		history.setTotalBurstTime(currentTime);
 		history.setTotalPowerConsumption(totalAccConsumption);
-
 		return history;
 	}
 
